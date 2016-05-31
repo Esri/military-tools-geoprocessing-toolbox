942ce4b07505a1da56254078c3b0c570c3a207fc
Matt Funk <mfunk@esri.com>
2016-05-31 13:38:29 -0700

    Updated LowestPointsTestCase.py and RadialLineOfSightTestCase.py

--- utils/test/visibility_tests/RadialLineOfSightTestCase.py
+++ utils/test/visibility_tests/RadialLineOfSightTestCase.py
@@ -1,33 +1,36 @@
 # coding: utf-8
-# -----------------------------------------------------------------------------
-# Copyright 2016 Esri
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# -----------------------------------------------------------------------------
+'''
+----------------------------------------------------------------------------
+Copyright 2016 Esri
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-# ==================================================
-# TableToPointTestCase.py
-# --------------------------------------------------
-# requirements:
-# * ArcGIS Desktop 10.X+ or ArcGIS Pro 1.X+
-# * Python 2.7 or Python 3.4
-#
-# author: ArcGIS Solutions
-# company: Esri
-#
-# ==================================================
-# history:
-# 5/18/2016 - DJH - initial creation
-# ==================================================
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-----------------------------------------------------------------------------
+
+==================================================
+TableToPointTestCase.py
+--------------------------------------------------
+requirements:
+* ArcGIS Desktop 10.X+ or ArcGIS Pro 1.X+
+* Python 2.7 or Python 3.4
+
+author: ArcGIS Solutions
+company: Esri
+
+==================================================
+history:
+5/18/2016 - DJH - initial creation
+5/31/2016 - MF - change error handling
+==================================================
+'''
 
 import unittest
 import arcpy
@@ -64,41 +67,39 @@ class RadialLineOfSightTestCase(unittest.TestCase):
         UnitTestUtilities.deleteScratch(Configuration.militaryScratchGDB)
 
     def test_radial_line_of_sight_desktop(self):
-        arcpy.AddMessage("Testing Radial Line Of Sight (Desktop).")
-        self.test_radial_line_of_sight(Configuration.military_DesktopToolboxPath)
-
-    def test_radial_line_of_sight_pro(self):
-        arcpy.AddMessage("Testing Radial Line Of Sight (Pro).")
-        self.test_radial_line_of_sight(Configuration.military_ProToolboxPath)
-
-    def test_radial_line_of_sight(self, toolboxPath):
+        ''' Test Radial Line Of Sight in ArcGIS Desktop'''
         try:
-            if Configuration.DEBUG == True: print("     RadialLineOfSightTestCase.test_Radial_line_of_sight")
-
-            arcpy.ImportToolbox(toolboxPath, "mt")
-            runToolMessage = "Running tool (Radial Line Of Sight)"
+            runToolMessage = ".....RadialLineOfSightTestCase.test_Radial_line_of_sight_desktop"
+            arcpy.ImportToolbox(Configuration.military_DesktopToolboxPath, "mt")
             arcpy.AddMessage(runToolMessage)
             Configuration.Logger.info(runToolMessage)
 
             arcpy.RadialLineOfSight_mt(self.observers, self.inputSurface, self.outputRLOS)
-            self.assertTrue(arcpy.Exists(self.outputRLOS))
+            self.assertTrue(arcpy.Exists(self.outputRLOS), "Output dataset does not exist or was not created")
 
             featureCount = int(arcpy.GetCount_management(self.outputRLOS).getOutput(0))
-#            self.assertEqual(featureCount, int(3584))
-            self.assertEqual(featureCount, int(3501))
-
-            '''rows = arcpy.SearchCursor(self.outputPoints)
-            row = rows.next()
-            while row:
-                elevation = row.Elevation
-                self.assertEqual(elevation, int(1123))
-                row = rows.next()'''
-
+            expectedFeatures = int(3501)
+            self.assertEqual(featureCount, expectedFeatures, "Expected %s features, but got %s" % (str(expectedFeatures), str(featureCount)))
 
         except arcpy.ExecuteError:
+            self.fail(runToolMessage + "\n" + arcpy.GetMessages())
             UnitTestUtilities.handleArcPyError()
 
-        except:
-            UnitTestUtilities.handleGeneralError()
+    def test_radial_line_of_sight_pro(self):
+        ''' Test Radial Line Of Sight in ArcGIS Pro'''
+        try:
+            runToolMessage = ".....RadialLineOfSightTestCase.test_Radial_line_of_sight_pro"
+            arcpy.ImportToolbox(Configuration.military_ProToolboxPath, "mt")
+            arcpy.AddMessage(runToolMessage)
+            Configuration.Logger.info(runToolMessage)
 
+            arcpy.RadialLineOfSight_mt(self.observers, self.inputSurface, self.outputRLOS)
+            self.assertTrue(arcpy.Exists(self.outputRLOS), "Output dataset does not exist or was not created")
 
+            featureCount = int(arcpy.GetCount_management(self.outputRLOS).getOutput(0))
+            expectedFeatures = int(3501)
+            self.assertEqual(featureCount, expectedFeatures, "Expected %s features, but got %s" % (str(expectedFeatures), str(featureCount)))
+
+        except arcpy.ExecuteError:
+            self.fail(runToolMessage + "\n" + arcpy.GetMessages())
+            UnitTestUtilities.handleArcPyError()
