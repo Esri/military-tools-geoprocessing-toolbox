c54b1d87700793471d883a879f47be70e0ec76b6
Matt Funk <mfunk@esri.com>
2016-05-31 07:59:40 -0700

    Fix TestKickStart.bat path to python.exe in Pro

--- utils/test/TestKickStart.bat
+++ utils/test/TestKickStart.bat
@@ -39,7 +39,7 @@ REM If you have BOTH versions of Python installed:
 
 ECHO Python 3.4 Tests ===============================
 REM py -3.4 TestRunner.py %LOG%
-"C:\Python34\python.exe" TestRunner.py %LOG%
+"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" TestRunner.py %LOG%
 
 ECHO Python 2.7 Tests ===============================
 REM py -2.7 TestRunner.py %LOG%
