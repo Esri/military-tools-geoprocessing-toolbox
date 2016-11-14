ff66f07288e0ac1e96f9d1f763edc1b51b38f15f
Matt Funk <mfunk@esri.com>
2016-11-14 15:28:07 -0800

    Fixes to Conversion tools
    Unit index fixes for 2-Point Line, Ellipse, Line Of Bearing.

--- utils/test/TestKickStart.bat
+++ utils/test/TestKickStart.bat
@@ -64,14 +64,15 @@ ECHO Python 3.4 Tests ===============================
 REM py -3.4 TestRunner.py %LOG%
 REM The location of python.exe will depend upon your installation
 REM of ArcGIS Pro. Modify the following line as necessary:
-"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" TestRunner.py %LOG%
+REM "C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" TestRunner.py %LOG%
+py -3.4 TestRunner.py %LOG%
 REM check if ArcGIS Pro/Python 3.4 tests failed
 IF %ERRORLEVEL% NEQ 0 (
    ECHO 'One or more tests failed'
 )
 ECHO Python 2.7 Tests ===============================
 REM py -2.7 TestRunner.py %LOG%
-py TestRunner.py %LOG%
+py -2.7 TestRunner.py %LOG%
 REM check if Desktop for ArcGIS/Python 2.7 tests failed
 IF %ERRORLEVEL% NEQ 0 (
    ECHO 'One or more tests failed'
