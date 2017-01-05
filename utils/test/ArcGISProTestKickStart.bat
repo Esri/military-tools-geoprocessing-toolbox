b4eb661c27aae1c81426c16a308e1ec364b93805
Matt Funk <mfunk@esri.com>
2017-01-05 15:29:49 -0800

    rename arcmap TBX, and launch tools with separate BAT files

--- utils/test/ArcGISProTestKickStart.bat
+++ utils/test/ArcGISProTestKickStart.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 rem ------------------------------------------------------------------------------
-rem  Copyright 2015 Esri
+rem  Copyright 2017 Esri
 rem  Licensed under the Apache License, Version 2.0 (the "License");
 rem  you may not use this file except in compliance with the License.
 rem  You may obtain a copy of the License at
@@ -13,7 +13,7 @@ rem  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 rem  See the License for the specific language governing permissions and
 rem  limitations under the License.
 rem ------------------------------------------------------------------------------
-rem  TestKickStart.bat
+rem  ArcGISProTestKickStart.bat
 rem ------------------------------------------------------------------------------
 rem  requirements:
 rem  * ArcGIS Desktop 10.X+ or ArcGIS Pro 1.X+
@@ -31,6 +31,7 @@ rem  5/9/2016: JH - initial creation
 rem  6/1/2016: MF - Add RangeRingUtils.py copy for testing
 rem  6/3/2016: MF - work on exit codes
 rem  12/22/2016: MF - change back to single execution for Jenkins builds
+rem  1/5/2017: MF - separate into Pro and Desktop test BAT
 rem ==================================================
 
 REM === TEST SETUP ===================================
@@ -40,53 +41,25 @@ ECHO Copying VisibilityUtilities.py
 COPY ..\..\toolboxes\scripts\VisibilityUtilities.py .\visibility_tests\VisibilityUtilities.py
 REM === TEST SETUP ===================================
 
-
 REM === LOG SETUP ====================================
 REM usage: set LOG=<defaultLogFileName.log>
 REM name is optional; if not specified, name will be specified for you
 set LOG=
 REM === LOG SETUP ====================================
 
-
-REM === SINGLE VERSION ==================================
-REM If you only have ONE version of Python installed
-REM uncomment the following lines
-REM =====================================================
-python TestRunner.py
+ECHO Testing with ArcGIS Pro ===============================
+REM The location of python.exe will depend upon your installation
+REM of ArcGIS Pro. Modify the following line as necessary:
+"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" TestRunner.py %LOG%
 IF %ERRORLEVEL% NEQ 0 (
    ECHO 'One or more tests failed'
 )
-REM === SINGLE VERSION ==================================
 
-
-rem REM === MULTIPLE VERSIONS ===============================
-rem REM If you have BOTH versions of Python installed use
-rem REM these lines
-rem REM =====================================================
-rem ECHO Python 3.4 Tests ===============================
-rem REM py -3.4 TestRunner.py %LOG%
-rem REM The location of python.exe will depend upon your installation
-rem REM of ArcGIS Pro. Modify the following line as necessary:
-rem REM "C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" TestRunner.py %LOG%
-rem py -3.4 TestRunner.py %LOG%
-rem REM check if ArcGIS Pro/Python 3.4 tests failed
-rem IF %ERRORLEVEL% NEQ 0 (
-rem    ECHO 'One or more tests failed'
-rem )
-rem ECHO Python 2.7 Tests ===============================
-rem py -2.7 TestRunner.py %LOG%
-rem REM check if Desktop for ArcGIS/Python 2.7 tests failed
-rem IF %ERRORLEVEL% NEQ 0 (
-rem    ECHO 'One or more tests failed'
-rem )
-rem REM === MULTIPLE VERSIONS ===============================
-
-
-REM === CLEANUP =========================================
+REM === TEST CLEANUP =========================================
 ECHO Removing RangeRingUtils.py ...
 DEL ".\distance_tests\RangeRingUtils.py"
 ECHO Removing VisibilityUtilities.py ...
 DEL ".\visibility_tests\VisibilityUtilities.py"
-REM === CLEANUP =========================================
+REM === TEST CLEANUP =========================================
 
 pause
