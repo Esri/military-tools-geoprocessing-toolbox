e9b9f3683282f3bb66a4aeacf710595abc259dd0
Chris Moore <csmooreacm@acm.org>
2018-04-27 12:10:14 -0400

    Add RangeRingFromMinimumAndMaximum

--- toolboxes/scripts/DistanceAndDirectionTools.py
+++ toolboxes/scripts/DistanceAndDirectionTools.py
@@ -42,7 +42,7 @@ class RangeRingsFromInterval(object):
     def __init__(self):
         self.label = u'Range Rings From Interval'
         self.description = u'Create a concentric circle from a center, with a number of rings, and the distance between rings.'
-        self.category = "Distance and Direction"        
+        self.category = u'Distance and Direction'
         self.canRunInBackground = False
         
     def getParameterInfo(self):
@@ -51,15 +51,13 @@ class RangeRingsFromInterval(object):
         param_1.name = u'Input_Center_Features'
         param_1.displayName = u'Input Center Features'
         param_1.parameterType = 'Required'
-        param_1.direction = 'Input'
+        param_1.direction = 'Input'        
+        param_1.datatype = u'Feature Set'  # Same as u'GPFeatureRecordSetLayer'
         
-        param_1.datatype = u'Feature Set'
-        # param_1.datatype = u'GPFeatureRecordSetLayer'
-        
+        # Set the Feature Set schema
         input_layer_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)),
                                           "layers",
-                                          "RelativeGRGInputPoint.lyr")
-                                          # "RangeRingInputObservers.lyrx")
+                                          "RangeRingInputObserversGDB.lyr")
         param_1.value = input_layer_file_path
 
         # Number_of_Rings
@@ -156,12 +154,12 @@ class RangeRingsFromInterval(object):
         optionalSpatialReference = parameters[7].value
         optionalSpatialReferenceAsText = str(parameters[7].value)
 
+        if optionalSpatialReferenceAsText == "#" or optionalSpatialReferenceAsText == "":
+            optionalSpatialReference = None
+
         # WORKAROUND (for Pro): clear layer selection (since last point is selected)
         # So tool will work on all points entered 
         arcpy.SelectLayerByAttribute_management(inputCenterFeatures, "CLEAR_SELECTION", None, None)
-
-        if optionalSpatialReferenceAsText == "#" or optionalSpatialReferenceAsText == "":
-            optionalSpatialReference = None
 
         # get/set environment
         arcpy.env.overwriteOutput = True
@@ -175,5 +173,141 @@ class RangeRingsFromInterval(object):
                                                    outputRingFeatures,
                                                    outputRadialFeatures,
                                                    optionalSpatialReference)
+        # Set output
+        return rr[0], rr[1]
+
+class RangeRingFromMinimumAndMaximum(object):
+
+    def __init__(self):
+        self.label = u'Range Rings From Minimum And Maximum'
+        self.description = u'Create a concentric circle from a center with two rings depicting a minimum range and a maximum range.'
+        self.category = u'Distance and Direction'
+        self.canRunInBackground = False
+
+    def getParameterInfo(self):
+        # Input_Center_Features
+        param_1 = arcpy.Parameter()
+        param_1.name = u'Input_Center_Features'
+        param_1.displayName = u'Input Center Features'
+        param_1.parameterType = 'Required'
+        param_1.direction = 'Input'
+        param_1.datatype = u'Feature Set'
+
+        # Set the Feature Set schema
+        input_layer_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)),
+                                          "layers",
+                                          "RangeRingInputObserversGDB.lyr")
+        param_1.value = input_layer_file_path
+
+        # Minimum_Range
+        param_2 = arcpy.Parameter()
+        param_2.name = u'Minimum_Range'
+        param_2.displayName = u'Minimum Range'
+        param_2.parameterType = 'Required'
+        param_2.direction = 'Input'
+        param_2.datatype = u'Double'
+        param_2.value = u'10'
+
+        # Maximum_Range
+        param_3 = arcpy.Parameter()
+        param_3.name = u'Maximum_Range'
+        param_3.displayName = u'Maximum Range'
+        param_3.parameterType = 'Required'
+        param_3.direction = 'Input'
+        param_3.datatype = u'Double'
+        param_3.value = u'100'
+
+        # Distance_Units
+        param_4 = arcpy.Parameter()
+        param_4.name = u'Distance_Units'
+        param_4.displayName = u'Distance Units'
+        param_4.parameterType = 'Required'
+        param_4.direction = 'Input'
+        param_4.datatype = u'String'
+        param_4.value = u'METERS'
+        param_4.filter.list = [u'METERS', u'KILOMETERS', u'MILES', u'NAUTICAL_MILES', u'FEET', u'US_SURVEY_FEET']
+
+        # Number_of_Radials
+        param_5 = arcpy.Parameter()
+        param_5.name = u'Number_of_Radials'
+        param_5.displayName = u'Number of Radials'
+        param_5.parameterType = 'Required'
+        param_5.direction = 'Input'
+        param_5.datatype = u'Long'
+        param_5.value = u'8'
+
+        # Output_Ring_Features
+        param_6 = arcpy.Parameter()
+        param_6.name = u'Output_Ring_Features'
+        param_6.displayName = u'Output Ring Features'
+        param_6.parameterType = 'Required'
+        param_6.direction = 'Output'
+        param_6.datatype = u'Feature Class'
+        param_6.value = u'%scratchGDB%\\rings'
+        param_6.symbology = os.path.join(os.path.dirname(os.path.dirname(__file__)),
+                                            "layers", "RangeRings.lyr")
+
+        # Output_Radial_Features
+        param_7 = arcpy.Parameter()
+        param_7.name = u'Output_Radial_Features'
+        param_7.displayName = u'Output Radial Features'
+        param_7.parameterType = 'Required'
+        param_7.direction = 'Output'
+        param_7.datatype = u'Feature Class'
+        param_7.value = u'%scratchGDB%\\radials'
+        param_7.symbology = os.path.join(os.path.dirname(os.path.dirname(__file__)),
+                                            "layers", "RangeRadials.lyr")
+
+        # Spatial_Reference
+        param_8 = arcpy.Parameter()
+        param_8.name = u'Spatial_Reference'
+        param_8.displayName = u'Spatial Reference'
+        param_8.parameterType = 'Optional'
+        param_8.direction = 'Input'
+        param_8.datatype = u'Spatial Reference'
+
+        return [param_1, param_2, param_3, param_4, param_5, param_6, param_7, param_8]
+
+    def isLicensed(self):
+        return True
+
+    def updateParameters(self, parameters):
+        validator = getattr(self, 'ToolValidator', None)
+        if validator:
+             return validator(parameters).updateParameters()
+
+    def updateMessages(self, parameters):
+        validator = getattr(self, 'ToolValidator', None)
+        if validator:
+             return validator(parameters).updateMessages()
+
+    def execute(self, parameters, messages):
+
+        inputCenterFeatures = parameters[0].value
+        inputMinimumRange = parameters[1].value
+        inputMaximumRange = parameters[2].value
+        inputDistanceUnits = parameters[3].value
+        inputNumberOfRadials = parameters[4].value
+        outputRingFeatures = parameters[5].value
+        outputRadialFeatures = parameters[6].value
+        optionalSpatialReference = parameters[7].value
+        optionalSpatialReferenceAsText = str(parameters[7].value)
+
+        if optionalSpatialReferenceAsText == "#" or optionalSpatialReferenceAsText == "":
+            optionalSpatialReference = None
+
+        # WORKAROUND (for Pro): clear layer selection (since last point is selected)
+        # So tool will work on all points entered 
+        arcpy.SelectLayerByAttribute_management(inputCenterFeatures, "CLEAR_SELECTION", None, None)
+
+        rr = RangeRingUtils.rangeRingsFromMinMax(inputCenterFeatures,
+                                                 inputMinimumRange,
+                                                 inputMaximumRange,
+                                                 inputDistanceUnits,
+                                                 inputNumberOfRadials,
+                                                 outputRingFeatures,
+                                                 outputRadialFeatures,
+                                                 optionalSpatialReference)
+
         # Set output
         return rr[0], rr[1]
